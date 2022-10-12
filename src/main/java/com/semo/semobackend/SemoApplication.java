9f1870508c5325057af3510843745cd05500102d
윤지영 <mer.toonine@gmail.com>
2022-10-12 21:55:23 +0900

    :memo: test2

--- src/main/java/com/semo/semobackend/SemoApplication.java
+++ src/main/java/com/semo/semobackend/SemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SemoApplication {
 
     // go semo!
+    // gogo!
     public static void main(String[] args) {
         SpringApplication.run(SemoApplication.class, args);
     }
