6e7acf7e2c715b4af5cfce090947e5dceba74361
윤지영 <mer.toonine@gmail.com>
2022-10-12 21:59:23 +0900

    :memo: branch test

--- src/main/java/com/semo/semobackend/SemoApplication.java
+++ src/main/java/com/semo/semobackend/SemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SemoApplication {
 
     // go semo!
-    // gogo!
+    // gogo~
     public static void main(String[] args) {
         SpringApplication.run(SemoApplication.class, args);
     }
