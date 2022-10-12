0c972a8d8099b19eeb94686873a97d0b2c102166
윤지영 <mer.toonine@gmail.com>
2022-10-12 21:15:56 +0900

    :memo init project

---
+++ src/main/java/com/semo/semobackend/SemoApplication.java
@@ -0,0 +1,13 @@
+package com.semo.semobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semo/semobackend/SemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semo.semobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
